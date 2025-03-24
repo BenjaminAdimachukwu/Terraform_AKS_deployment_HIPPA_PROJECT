b20e3a5488b5a77cef21c42d20c5c19790f47a58
Benjamin Adimachukwu <adimachiben@gmail.com>
2025-03-24 11:54:50 +0100

    first commit

--- 0_provider.tf
+++ 0_provider.tf
@@ -20,16 +20,16 @@ terraform {
 provider "azurerm" {
   features {}
 
-  client_id       = var.client_id   # 528c66da-9370-4e4c-98b0-732f39850a73
-  client_secret   = var.client_secret  # ZrN8Q~vh67Lc2RdUEPGDIa1g-pdKmkPLnbbZhav6
-  tenant_id       = var.tenant_id      # 9c15087d-c708-496f-a176-b6c84e265a21
-  subscription_id = var.subscription_id  # 235c318b-f5da-4a4a-b1e5-756b4b5a9a29
+  client_id       = var.client_id   
+  client_secret   = var.client_secret  
+  tenant_id       = var.tenant_id      
+  subscription_id = var.subscription_id  
 }
 
-variable "client_id" {}
-variable "client_secret" {}
-variable "tenant_id" {}
-variable "subscription_id" {}
+variable "client_id" {}  #7fb8c580-799d-496d-9d53-72a81451cb30
+variable "client_secret" {} #0304b316-167c-4389-94d6-cdf28407d1a4
+variable "tenant_id" {} #89756a7e-858c-4132-85e8-16cbddc4eb55
+variable "subscription_id" {} #b817b533-8f44-41cd-bf4e-1ea318323387
 
 # terraform-azure-pipeline  is the name of the ( App Registration )
 # permission for the App Registration 
